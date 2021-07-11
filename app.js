511f28f4534de13597242885b9cde27b415e0f4f
Magdi Tiea <magditiea@gmail.com>
2021-07-11 10:17:22 -0400

    route changed

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ function(accessToken, refreshToken, profile, cb) {
 ));
 
 
-app.get("/", function (req, res) {
+app.get("/host=serene-savannah-28526.herokuapp.com", function (req, res) {
   res.render("home");
 });
 // authentication with google using the user strategy
